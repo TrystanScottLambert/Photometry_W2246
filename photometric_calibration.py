90c10e1f6e8ad580363a1390ccd546c50bbeaafc
Trystan Lambert <trystanscottlambert@gmail.com>
2023-05-23 16:39:21 -0400

    Tests.

--- photometric_calibration.py
+++ photometric_calibration.py
@@ -1,34 +1,259 @@
+"""
+Photometrically calibrating image by performing a reduction of the chi2 value for 
+various color terms from a matching catalog and instrumental magnitudes from the host
+telescope.
+"""
+
+from dataclasses import dataclass
+from functools import cached_property, partial
 import numpy as np
-import matplotlib.pyplot as plt
 from astropy.table import Table
-from astropy.stats import SigmaClip, sigma_clipped_stats, sigma_clip
 
-filename = "GMOS_PANSTARRS.dat"
-def calibrate_gmos_magnitudes(filename):
+def photometric_transformation(m_cat: np.ndarray, constants: np.ndarray, color_terms: np.ndarray):
     """
-    This code is to perform the photometric calibration using a color transformation method.
-    
-    Perform photometric calibration for a set of magnitudes with respect to a 'reference' catalog.
-
-    Parameters:
-        catalog data: The name of the FITS or ASCII file containing the data.
- 
-    Returns:
-           containing:
-            -  the calibration coefficients for each magnitude.
-            - the calibrated magnitudes for each magnitude.
-            - the magnitude residuals for each magnitude.
+    General form of the transformation from instrumental
+    magnitude to photometrically corrected mag.
     """
+    constant_terms = []
+    for i, color_term in enumerate(color_terms):
+        constant_terms.append(constants[i]*color_term)
+        constant_term = np.sum(constant_terms)
+    return m_cat + constant_term
 
+@dataclass
+class FilterMag:
+    """Representation of a color observation"""
+    name: str
+    array: np.ndarray[float]
+    array_err: np.ndarray[float]
+
+@dataclass
+class Color:
+    """
+    Represents the difference between two given colors. 
+    The value will always be color_1 - color_2.
+    """
+    name: str
+    filter_1: FilterMag
+    filter_2: FilterMag
+
+    @property
+    def value(self):
+        """Calculates the color"""
+        return self.filter_1.array - self.filter_2.array
+
+def get_unique_filters(list_filters: list[FilterMag]) -> list[FilterMag]:
+    """Returns a unique list of given filters."""
+    unique_filters = []
+    for _filter in list_filters:
+        if _filter not in unique_filters:
+            unique_filters.append(_filter)
+    return unique_filters
+
+
+@dataclass
+class Settings:
+    """All the inputs required to run the calibraton."""
+    observed_mag: FilterMag
+    catalog_mag: FilterMag
+    colors: list[Color]
+
+    @cached_property
+    def sigma_squared(self) -> np.ndarray:
+        """Determines the error term, sigma squared, from the errors of the given magnitudes."""
+        filters = self._get_all_filters()
+        sigma_array = np.zeros(len(self.observed_mag.array_err))
+        for _filter in filters:
+            sigma_array += _filter.array_err**2
+        return sigma_array
+
+    @cached_property
+    def color_terms(self) -> np.ndarray:
+        """Returns the array of color terms."""
+        val = [np.ones(len(self.colors[0].value))]
+        for color in self.colors:
+            val.append(color.value)
+        return val
+
+    @property
+    def delta_m(self) -> np.ndarray:
+        """
+        The difference between the observed magnitudes and the catalog magnitudes.
+        """
+        return self.observed_mag.array - self.catalog_mag.array
+
+    def _get_all_filters(self):
+        """
+        Returns all the filters which are being used in the calculation, 
+        including from the colors.
+        """
+        filters = []
+        filters.append(self.observed_mag)
+        filters.append(self.catalog_mag)
+        for color in self.colors:
+            filters.append(color.filter_1)
+            filters.append(color.filter_2)
+        return get_unique_filters(filters)
+
+def create_matrix(settings: Settings) -> np.array:
+    """
+    Creates the A matrix associated with the color terms. 
+    """
+    a_matrix = []
+    for color_term in settings.color_terms:
+        row = []
+        for other_color_term in settings.color_terms:
+            row.append(np.sum((color_term * other_color_term)/settings.sigma_squared))
+        a_matrix.append(row)
+    return np.array(a_matrix)
+
+def create_vector(settings: Settings) -> np.array:
+    """
+    Creates the b vector which is associated with the 
+    """
+    b_vector = []
+    for color_term in settings.color_terms:
+        b_vector.append(
+            np.sum(((settings.delta_m) * color_term) / (settings.sigma_squared))
+        )
+    return np.array(b_vector)
+
+def calculate_constants(a_matrix:np.matrix, b_vector: np.ndarray) -> np.ndarray:
+    """Calculates the values of the constants."""
+    return np.linalg.inv(a_matrix).dot(b_vector)
+
+def get_photometric_transformation(settings: Settings) -> callable:
+    """Determines the transformation for converting instrumental mags into catalog mags."""
+    a_matrix = create_matrix(settings)
+    b_vector = create_vector(settings)
+    constants = calculate_constants(a_matrix, b_vector)
+    transformation = partial(photometric_transformation, constants = constants, color_terms = settings.color_terms)
+    return transformation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    filename = "GMOS_PANSTARRS.dat"
     # Load the data
     gmos_panss = Table.read(filename, format='ascii')
 
-    r_gmos = gmos_panss['mag_r']
+    r_gmos = gmos_panss['rmag']
     r_pan = gmos_panss['rApMag']
     g_pan = gmos_panss['gApMag']
     i_pan = gmos_panss['iApMag']
 
-    re_gmos = gmos_panss['magerr_r']
+    re_gmos = gmos_panss['rmagerr']
     re_pan = gmos_panss['rApMagErr']
     ge_pan = gmos_panss['gApMagErr']
     ie_pan = gmos_panss['iApMagErr']
@@ -48,16 +273,16 @@ def calibrate_gmos_magnitudes(filename):
     A33 = np.sum((i_pan-r_pan)**2/sigma)  # D3
 
     A = np.array([[A11, A12, A13],
-                  [A21, A22, A23],
-                  [A31, A32, A33]])
+                [A21, A22, A23],
+                [A31, A32, A33]])
 
     b1 = np.sum((r_gmos-r_pan)/sigma)  # B
     b2 = np.sum((r_gmos-r_pan)*((g_pan-r_pan)/sigma))  # A
     b3 = np.sum((r_gmos-r_pan)*((i_pan-r_pan)/sigma))  # D
 
-    B = np.array([[b1],
-                  [b2],
-                  [b3]])
+    B = np.array([b1,
+                b2,
+                b3])
 
     const = np.linalg.inv(A).dot(B)
     C1 = float(const[0])
@@ -73,37 +298,3 @@ def calibrate_gmos_magnitudes(filename):
 
     # Calculate the difference between the two calibrated GMOS magnitudes
     r_diff = cal_gmos_r - cal_gmos_pan_r
-
-    # Calculate statistics of the difference
- 
-    mean, median, std = sigma_clipped_stats(r_diff)
-
-    # Print the statistics
-    print('mean:', mean, 'median:', median, 'standard deviation:', std)
-
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
-
-    # Plot on the first panel
-    axs[0].plot(cal_gmos_pan_r, r_gmos, 'o', markersize=3)
-    axs[0].set_xlabel('Calibrated_GMOS_PAN r')
-    axs[0].set_ylabel('GMOS r')
-
-    # Plot on the second panel
-    axs[1].plot(cal_gmos_r, r_diff, 'o', markersize=3)
-    axs[1].set_xlabel('Calibrated GMOS r')
-    axs[1].set_ylabel('Calibrated GMOS r - Calibrated GMOS PAN r')
-
-    # Plot on the third panel
-    axs[2].plot(r_pan,r_gmos-r_pan, 'o', markersize=3)
-    axs[2].set_xlabel('PANSTARRS r')
-    axs[2].set_ylabel('GMOS r - PANSTARRS r')
-
-    # Adjust the space between subplots
-    plt.subplots_adjust(wspace=0.4)
-
-    #   Save the figure
-    plt.savefig('calibrated_magnitudes.png', dpi=300)
-
-    # Show the plot
-    plt.show()
-calibrate_gmos_magnitudes(filename)
